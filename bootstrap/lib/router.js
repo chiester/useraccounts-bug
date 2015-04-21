527a9fa99d2e1d9e9a4137f1e59a4c6f11fadbd8
Christopher Hiester <chris.hiester@gmail.com>
2015-04-21 01:25:59 -0400

    add route change to force login to be home

--- bootstrap/lib/router.js
+++ bootstrap/lib/router.js
@@ -1,21 +1,33 @@
 Router.configure({
-    layoutTemplate: 'masterLayout',
-    loadingTemplate: 'loading',
-    notFoundTemplate: 'pageNotFound',
-    yieldTemplates: {
-        nav: {to: 'nav'},
-        footer: {to: 'footer'},
-    }
+	layoutTemplate: 'masterLayout',
+	loadingTemplate: 'loading',
+	notFoundTemplate: 'pageNotFound',
+	yieldTemplates: {
+		nav: {
+			to: 'nav'
+		},
+		footer: {
+			to: 'footer'
+		},
+	}
 });
 
 Router.map(function() {
-    this.route('home', {
-        path: '/',
-    });
+	this.route('home', {
+		path: '/',
+		onBeforeAction: function() {
+			if (Meteor.user()) {
+				Router.go('private');
+			} else {
+        Router.go('home');
+      }
+			this.next();
+		}
+	});
 
-    this.route('private');
+	this.route('private');
 });
 
 Router.plugin('ensureSignedIn', {
-  only: ['private']
+	only: ['private']
 });
